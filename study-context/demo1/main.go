8fdb62aca75f0ff7512048fef8680d034d2ae5e0
shan <shanqi@goldensecurity.com.cn>
2023-11-01 17:15:30 +0800

    add context demo

---
+++ study-context/demo1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+func main() {
+	// 创建一个带有取消功能的上下文
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	// 模拟一个耗时操作，在 3 秒后取消上下文
+
+	//time.Sleep(3 * time.Second)
+
+	// 执行操作并检查上下文状态
+	if ctx != nil {
+		select {
+		case <-ctx.Done():
+			fmt.Println("操作被取消:", ctx.Err())
+		default:
+			fmt.Println("操作正常完成")
+			// 这里可以添加具体的操作代码
+		}
+	} else {
+		fmt.Println("上下文为空")
+	}
+}
